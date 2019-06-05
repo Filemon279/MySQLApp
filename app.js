94845cac64950c3993f7243a2855ad6ca176f585
filemon <filemon279@o2.pl>
2019-06-05 17:27:30 +0200

    Updated

--- app.js
+++ app.js
@@ -6,18 +6,53 @@ var mysql = require('mysql');
 
 var loggedIn = [];
 
-var port = 3001;
+var port = 3000;
 
-var con = mysql.createConnection({
+var db_config = {
   host: "serwer1895835.home.pl",
   user: "29158242_projektsql",
   password: "projektsql",
   database: "29158242_projektsql"
-});
+};
 
-con.connect(function(err) {
-  if (err) throw err;
-});
+var errorsTranslator = [];
+for(var i = 0; i<2000; i++)
+
+errorsTranslator[1062] = "W bazie znajduje się już taki klucz"
+errorsTranslator[1451] = "Nie można usunąć rekordu ponieważ jest powiązany z innym rekordem"
+
+function dissplayError(error)  {
+  if(errorsTranslator[error.errno]) return errorsTranslator[error.errno]
+  else return "Niestety wystąpił niezdefiniowany błąd MySQL, który głosi: "+error.sqlMessage;
+}
+
+
+
+var con;
+
+function handleDisconnect() {
+  con = mysql.createConnection(db_config);
+                                                  
+  con.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                     
+                                          
+  con.on('error', function(err) {
+    //console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                 
+    }
+  });
+}
+
+handleDisconnect()
+
+
 
 app.set('view engine', 'ejs');
 
@@ -54,42 +89,41 @@ io.on('connection', function(socket){
 
     function emitLogs(userId, limit) {
       con.query(`SELECT * FROM logi WHERE o_id='${userId}' ORDER BY data DESC LIMIT ${limit}`, function (err, result, fields) {
-            if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+            if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         if(result.length > 0) socket.emit('userLogs', JSON.stringify(result))
         });
     }
 
     function getAllGroups(){
-        con.query(`SELECT * FROM grupa`, function (err, result, fields) {
-            if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+        con.query(`SELECT * FROM grupa JOIN sala on s_id = ids JOIN pracownik on p_id = idp JOIN osoba on o_id = ido;`, function (err, result, fields) {
+            if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         socket.emit('groupAllList', JSON.stringify(result))
         });
     }
     function getAllRooms(){
       con.query(`SELECT * FROM sala`, function (err, result, fields) {
-            if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+            if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         socket.emit('roomList', JSON.stringify(result))
       });
     }
 
     function allSubjectListRequest(){
-      con.query(`SELECT numer AS sala, przedmiot, data, osoba.imie AS imie, osoba.nazwisko AS nazwisko, wykladowca.imie AS wImie, wykladowca.nazwisko AS wNazwisko FROM uczestnik JOIN grupa on g_id = idg JOIN osoba ON o_id = ido JOIN
+      con.query(`SELECT numer AS sala, przedmiot, idu, data, osoba.imie AS imie, osoba.nazwisko AS nazwisko, wykladowca.imie AS wImie, wykladowca.nazwisko AS wNazwisko FROM uczestnik JOIN grupa on g_id = idg JOIN osoba ON o_id = ido JOIN
       pracownik ON p_id = idp JOIN osoba AS wykladowca ON pracownik.o_id = wykladowca.ido JOIN sala on s_id = ids`, function (err, result, fields) {
      if (err) throw err;
      socket.emit('subjectList', JSON.stringify(result))
      });
     }
-
     socket.on('teacherListRequest', function(){
       con.query(`SELECT * FROM osoba JOIN pracownik on o_id = ido;`, function (err, result, fields) {
-            if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+            if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         socket.emit('teacherList', JSON.stringify(result))
         });
     })
 
     socket.on('adminLogListRequest', function(limit){
-      con.query(`SELECT * FROM logi JOIN osoba ON o_id = ido ORDER BY data DESC LIMIT ${limit}`, function (err, result, fields) {
-      if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+      con.query(`SELECT * FROM logi JOIN osoba ON o_id = ido JOIN sala on s_id = ids ORDER BY data DESC LIMIT ${limit}`, function (err, result, fields) {
+      if (err) {socket.emit('errorMsg', dissplayError(err)); return}
       socket.emit('adminLogList', JSON.stringify(result))
       });
     });
@@ -100,10 +134,26 @@ io.on('connection', function(socket){
 
     socket.on('userListRequest', function(){
         con.query(`SELECT * FROM osoba`, function (err, result, fields) {
-          if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+          if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         socket.emit('userList', JSON.stringify(result))
         });
       });
+
+      socket.on('removeParticipant', function(id){
+        con.query(`DELETE FROM uczestnik WHERE idu = ${id}`, function (err, result, fields) {
+            console.log(err)
+              if (err) {socket.emit('errorMsg', dissplayError(err)); return}
+              allSubjectListRequest();
+          });
+      })
+
+      socket.on('removeRoom', function(id){
+        con.query(`DELETE FROM sala WHERE ids = ${id}`, function (err, result, fields) {
+            console.log(err)
+              if (err) {socket.emit('errorMsg', dissplayError(err)); return}
+              getAllRooms();
+          });
+      })
 
       socket.on('getRooms', function(){
         getAllRooms();
@@ -112,7 +162,10 @@ io.on('connection', function(socket){
       socket.on('addRoom', function(data){
         var roomData = JSON.parse(data);
         con.query(`INSERT INTO sala (numer, st_dostepu) VALUES ('${roomData.number}', '${roomData.accessLevel}')`, function (err, result, fields) {
-          if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+          if (err) {
+            console.log(err);
+            socket.emit('errorMsg', dissplayError(err)); return
+          }
           getAllRooms();
         });
       });
@@ -122,9 +175,9 @@ io.on('connection', function(socket){
      socket.on('addStudent', function(data){
         var userData = JSON.parse(data);
         con.query(`INSERT INTO osoba (imie, nazwisko, st_dostepu) VALUES ('${userData.name}', '${userData.surname}', '${userData.accessLevel}')`, function (err, result, fields) {
-          if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+          if (err) {socket.emit('errorMsg', dissplayError(err)); return}
           con.query(`INSERT INTO student (o_id, nr_indeksu) VALUES ('${result.insertId}', '${userData.indexNumber}')`, function (err, result, fields) {
-				if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+				if (err) {socket.emit('errorMsg', dissplayError(err)); return}
 			});
         });
       });
@@ -132,9 +185,9 @@ io.on('connection', function(socket){
       socket.on('addTeacher', function(data){
         var userData = JSON.parse(data);
         con.query(`INSERT INTO osoba (imie, nazwisko, st_dostepu) VALUES ('${userData.name}', '${userData.surname}', '${userData.accessLevel}')`, function (err, result, fields) {
-          if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+          if (err) {socket.emit('errorMsg', dissplayError(err)); return}
           con.query(`INSERT INTO pracownik (o_id, stanowisko) VALUES ('${result.insertId}', '${userData.position}')`, function (err, result, fields) {
-				if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+				if (err) {socket.emit('errorMsg', dissplayError(err)); return}
 			});
         });
       });
@@ -143,7 +196,7 @@ io.on('connection', function(socket){
         var groupData = JSON.parse(data);
         con.query(`INSERT INTO grupa (przedmiot, data, licznosc, s_id, p_id) VALUES ('${groupData.subject}', '${groupData.date}',
         '${groupData.count}', '${groupData.roomId}', '${groupData.teacherId}')`, function (err, result, fields) {
-            if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+            if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         getAllGroups();
         });
       });
@@ -151,7 +204,7 @@ io.on('connection', function(socket){
       socket.on('addParticipant', function(data){
         var partData = JSON.parse(data);
         con.query(`INSERT INTO uczestnik (g_id, o_id) VALUES ('${partData.g_id}', '${partData.s_id}')`, function (err, result, fields) {
-            if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+            if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         allSubjectListRequest();
         });
       });
@@ -161,17 +214,11 @@ io.on('connection', function(socket){
       });
 
       socket.on('userPermissionsRequest', function(userId){
-        con.query(`SELECT ids FROM osoba JOIN sala WHERE ido='${userId}' AND osoba.st_dostepu >= sala.st_dostepu;`, function (err, result, fields) {
-              if (err) {socket.emit('errorMsg', err.sqlMessage); return}
-          con.query(`SELECT ids FROM osoba JOIN uczestnik ON o_id = ido JOIN grupa ON g_id = idg JOIN sala ON s_id = ids WHERE ido='${userId}'`, function (err, secResult, fields) {
-                if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+        con.query(`(SELECT ids FROM osoba JOIN sala WHERE ido='${userId}' AND osoba.st_dostepu >= sala.st_dostepu) UNION
+        (SELECT ids FROM osoba JOIN uczestnik ON o_id = ido JOIN grupa ON g_id = idg JOIN sala ON s_id = ids WHERE ido='${userId}')`, function (err, result, fields) {
+              if (err) {socket.emit('errorMsg', dissplayError(err)); return}
             result = JSON.parse(JSON.stringify(result))
-            secResult = JSON.parse(JSON.stringify(secResult))
-            for(var i = 0; i<secResult.length; i++){
-              result[result.length] = secResult[i];
-            }
             socket.emit('userPermissions', JSON.stringify(result))
-            });
           });
       });
 
@@ -181,7 +228,7 @@ io.on('connection', function(socket){
 
       socket.on('getRooms', function(){
         con.query(`SELECT * FROM sala`, function (err, result, fields) {
-            if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+            if (err) {socket.emit('errorMsg', dissplayError(err)); return}
         socket.emit('roomList', JSON.stringify(result))
         });
       });
@@ -193,22 +240,16 @@ io.on('connection', function(socket){
         var data = JSON.parse(jData);
         var action = "Tried to open door, no access"
 
-        con.query(`SELECT ids FROM osoba JOIN sala WHERE ido='${data.userId}' AND osoba.st_dostepu >= sala.st_dostepu;`, function (err, result, fields) {
-              if (err) {socket.emit('errorMsg', err.sqlMessage); return}
-          con.query(`SELECT ids FROM osoba JOIN uczestnik ON o_id = ido JOIN grupa ON g_id = idg JOIN sala ON s_id = ids WHERE ido='${data.userId}'`, function (err, secResult, fields) {
-                if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+        con.query(`(SELECT ids FROM osoba JOIN sala WHERE ido='${data.userId}' AND osoba.st_dostepu >= sala.st_dostepu) UNION
+        (SELECT ids FROM osoba JOIN uczestnik ON o_id = ido JOIN grupa ON g_id = idg JOIN sala ON s_id = ids WHERE ido='${data.userId}')`, function (err, result, fields) {
+              if (err) {socket.emit('errorMsg', dissplayError(err)); return}
             result = JSON.parse(JSON.stringify(result))
-            secResult = JSON.parse(JSON.stringify(secResult))
-            for(var i = 0; i<secResult.length; i++){
-              result[result.length] = secResult[i];
-            }
             if(result == 0) ;
             if(result.filter(e => e.ids == data.doorId).length > 0) {action = "Open door"}
             con.query(`INSERT INTO logi (data, akcja, s_id, o_id) VALUES (now(), '${action}', ${data.doorId}, ${data.userId})`, function (err, result, fields) {
-                  if (err) {socket.emit('errorMsg', err.sqlMessage); return}
+                  if (err) {socket.emit('errorMsg', dissplayError(err)); return}
               else emitLogs(data.userId,data.limit);
             })
-            });
           });
         
 
